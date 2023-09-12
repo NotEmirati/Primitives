97cc1d1ee0699f9c38794b3c6a5763ba6ed80492
NotEmirati <143645269+NotEmirati@users.noreply.github.com>
2023-09-12 14:20:06 +0400

    Update addition_with_random.py
    - added exceed 100 path

--- addition_with_random.py
+++ addition_with_random.py
@@ -3,8 +3,11 @@ import random
 def addition(firstInput:int, secondInput:int):
     randomInput = random.randint(0,100)
     total = firstInput + secondInput + randomInput
+    if total > 100:
+        print('Total exceeds 100')
+    else:
+        print('The task is complete. The result is:')
+        print(total)
     return total
 
-total = 0
-
-print(addition(5,6))
+addition(20,10)

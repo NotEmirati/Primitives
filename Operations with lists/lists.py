33742ad9ce7c7d5b323c9c0ac8ae027b514f25b9
NotEmirati <143645269+NotEmirati@users.noreply.github.com>
2023-09-14 09:00:06 +0400

    Update lists.py

--- Operations with lists/lists.py
+++ Operations with lists/lists.py
@@ -1,3 +1,14 @@
+listName = [1, 2, 3, 4, 5]
+
 #sums all elements in the list
-def sum_list(listName):
+def sumList(listName):
   return sum(listName)
+
+#adds an element to the list
+def appendList(n:int):
+    listName.append(n)
+  
+#extends the list with another list and a tuple
+def extendList(l: list, t: tuple):
+    listName.extend(l)
+    listName.extend(t)

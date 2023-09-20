1a1b89a60ccbee2e09160d7129a73bfc2e374c40
NotEmirati <143645269+NotEmirati@users.noreply.github.com>
2023-09-20 09:34:56 +0400

    Update strings.py

--- Operations with strings/strings.py
+++ Operations with strings/strings.py
@@ -1,4 +1,18 @@
-#Joins elements with ' ' into one string.
+# joins elements with ' ' into one string
 def joinString(s:str):
     phrase = ' '.join(s)
     return phrase
+
+# removes characters from the string
+def removechar(string):
+    chars = 'AEIOUaeiou'
+    result = ''
+    for chars in string:
+        if chars not in chars:
+          result += chars
+    print(removechar('lorem ipsum'))
+    return result
+    
+
+
+

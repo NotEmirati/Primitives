a0cd16df08a98f574c7a050c4bfb56daef1b028d
NotEmirati <143645269+NotEmirati@users.noreply.github.com>
2023-10-06 05:25:10 +0400

    Update tuples.py

--- Operations with tuples/tuples.py
+++ Operations with tuples/tuples.py
@@ -1,3 +1,7 @@
 # returns number of elements in a given tuple
 def count_tuple_element(t: tuple, e: str):
   return t.count('e')
+
+# returns the first index of an element in a given tuple
+def index_tuple_element(t: tuple, e: str):
+    return t.index('e')

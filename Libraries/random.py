9913a827161c11a01c357c62d5ca6055978462e9
NotEmirati <143645269+NotEmirati@users.noreply.github.com>
2023-10-08 12:04:49 +0400

    Update random.py

--- Libraries/random.py
+++ Libraries/random.py
@@ -1,4 +1,9 @@
 import random 
 
+# returns random default number
 def generate_random():
     return random.random()
+
+# returns a random element of a given iterable
+def pick_random(l: list):
+    return random.choice(l)

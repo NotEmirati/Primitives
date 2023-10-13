c912431e8b0bb36123cee46725c879af4238705c
Denis Polozov <143645269+NotEmirati@users.noreply.github.com>
2023-10-13 09:37:46 +0400

    Update pandas.py

--- Libraries/pandas.py
+++ Libraries/pandas.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 
-# create DataFrame, assign it to variable, print it
+# create DataFrame, assign it to variable, print the dataframe and types for values in columns
 dict = {'FirstName': ['Alex', 'Mike', 'John'],
         'LastName': ['Michaels', 'Jackson', 'Sage'],
         'Age': [18, 25, 33]}
 
 df = pd.DataFrame(dict)
 print(df)
+print(df.dtypes)

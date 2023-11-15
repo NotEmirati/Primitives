19d8e85017bdca4764b18b2f68843124a58c1d6c
Denis Polozov <143645269+NotEmirati@users.noreply.github.com>
2023-11-15 18:38:24 +0400

    Update sets.py

--- Operations with sets/sets.py
+++ Operations with sets/sets.py
@@ -9,3 +9,7 @@ def sorted_set(givenSet: set):
 # return the l
 def sum_set(givenSet: set):
     return sum(givenSet)
+
+# return the smallest element of a set. Can be used for any iterable, including several iterables, can have optional args: 'key', 'iterables', 'default'.
+def min_value(set: set):
+    return min(set)

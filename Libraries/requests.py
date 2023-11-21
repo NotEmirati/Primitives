676dae614b26a4e8c209698e7dc3022560fff79e
Denis Polozov <143645269+NotEmirati@users.noreply.github.com>
2023-11-21 12:26:25 +0400

    Update requests.py

--- Libraries/requests.py
+++ Libraries/requests.py
@@ -17,3 +17,12 @@ def check_connection(s: str):
         print('Not Found.')
 
 print(response.url)
+
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+response = requests.get('https://api.github.com / user, ',
+                        auth=HTTPBasicAuth('user', 'pass'))
+
+print(response)

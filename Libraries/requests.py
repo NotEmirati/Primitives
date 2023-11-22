dfc60351e4f90df2a4c04ea8b681a594972d56cd
Denis Polozov <143645269+NotEmirati@users.noreply.github.com>
2023-11-22 16:50:25 +0400

    Update requests.py

--- Libraries/requests.py
+++ Libraries/requests.py
@@ -24,5 +24,19 @@ from requests.auth import HTTPBasicAuth
 
 response = requests.get('https://api.github.com / user, ',
                         auth=HTTPBasicAuth('user', 'pass'))
-
 print(response)
+
+# import requests module 
+import requests 
+  
+# create a session object 
+s = requests.Session() 
+  
+# make a get request 
+s.get('https://httpbin.org/cookies/set/sessioncookie/123456789') 
+  
+# again make a get request 
+r = s.get('https://httpbin.org/cookies') 
+  
+# check if cookie is still set 
+print(r.text) 
